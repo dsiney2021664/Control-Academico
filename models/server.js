b4fa084164b8b3dfc7777eb0ba11202da6138062
dsiney2021664 <dsiney-2021664@kinal.edu.gt>
2024-02-15 22:46:28 -0600

    Se agrego el crud completo de usuarios con 2 roles, uno de estudiante y uno de maestro

--- models/server.js
+++ models/server.js
@@ -2,27 +2,34 @@ const express = require('express');
 const cors = require('cors');
 const { dbConnection } = require('../db/config');
 
-class Server{
-    constructor(){
+class Server {
+
+    constructor() {
         this.app = express();
         this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios';
 
         this.conectarDB();
         this.middlewares();
-        
+        this.routes();
+
     }
 
-    async conectarDB(){
+    async conectarDB() {
         await dbConnection();
     }
 
-    middlewares(){
+    middlewares() {
         this.app.use(express.static('public'));
         this.app.use(cors());
         this.app.use(express.json());
     }
 
-    listen(){
+    routes() {
+        this.app.use(this.usuariosPath, require('../routes/user.routes'))
+    }
+
+    listen() {
         this.app.listen(this.port, () => {
             console.log('Servidor ejecutado y escuchando en el puerto', this.port);
         });
---
+++ models/usuario.js
@@ -0,0 +1,27 @@
+const { Schema, model} = require('mongoose');
+
+const UsuarioSchema = Schema ({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es obligatorio']
+    },
+    correo: {
+        type: String,
+        required: [true, 'El correo es obligatorio']
+    },
+    password: {
+        type: String,
+        required: [true, 'El password es obligatorio']
+    },
+    role:{
+        type: String,
+        required: true,
+        enum: ["TEACHER_ROLE", "STUDENT_ROLE"]
+    },
+    estado:{
+        type: Boolean,
+        default: true
+    }
+});
+
+module.exports = model('Usuario', UsuarioSchema);
